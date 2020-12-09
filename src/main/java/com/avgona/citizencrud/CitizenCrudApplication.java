3d2a77170bb460ffdf04b85cde3b9c3ff121e6ec
Avgon <Avgona2017@yandex.com>
2020-12-09 22:30:46 +0100

    test

--- src/main/java/com/avgona/citizencrud/CitizenCrudApplication.java
+++ src/main/java/com/avgona/citizencrud/CitizenCrudApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CitizenCrudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CitizenCrudApplication.class, args);
 	}
-
 }
